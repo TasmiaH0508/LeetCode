477f627b4140f1bd94bbf9028001a10e91da73af
TasmiaH0508 <tasmiahaque2004@gmail.com>
2025-05-13 21:35:00 +0800

    Time: 66 ms (72.15%), Space: 45.5 MB (86.08%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,28 @@
+class Solution {
+public int lengthAfterTransformations(String s, int t) {
+    final int MOD = 1_000_000_007;
+    int[] freqMap = new int[26];
+    
+    for (char c : s.toCharArray()) {
+        freqMap[c - 'a']++;
+    }
+
+    for (int i = 0; i < t; i++) {
+        int[] newFreqMap = new int[26];
+        newFreqMap[0] = freqMap[25];
+        for (int j = 1; j < 26; j++) {
+            newFreqMap[j] = freqMap[j - 1];
+        }
+        freqMap = newFreqMap;
+        freqMap[1] = (freqMap[1] + freqMap[0]) % MOD;
+    }
+
+    long sum = 0;
+    for (int val : freqMap) {
+        sum = (sum + val) % MOD;
+    }
+
+    return (int) sum;
+}
+
+}
