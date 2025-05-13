93046f8f2119fdb53b6e1c31e5afdfe2de10b355
TasmiaH0508 <tasmiahaque2004@gmail.com>
2025-05-13 21:41:34 +0800

    Time: 65 ms (72.15%), Space: 45.3 MB (97.47%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
@@ -1,28 +1,28 @@
 class Solution {
-public int lengthAfterTransformations(String s, int t) {
-    final int MOD = 1_000_000_007;
-    int[] freqMap = new int[26];
-    
-    for (char c : s.toCharArray()) {
-        freqMap[c - 'a']++;
-    }
+    public int lengthAfterTransformations(String s, int t) {
+        final int MOD = 1_000_000_007;
+        int[] freqMap = new int[26];
 
-    for (int i = 0; i < t; i++) {
-        int[] newFreqMap = new int[26];
-        newFreqMap[0] = freqMap[25];
-        for (int j = 1; j < 26; j++) {
-            newFreqMap[j] = freqMap[j - 1];
+        for (char c : s.toCharArray()) {
+            freqMap[c - 'a']++;
         }
-        freqMap = newFreqMap;
-        freqMap[1] = (freqMap[1] + freqMap[0]) % MOD;
+
+        for (int i = 0; i < t; i++) {
+            int[] newFreqMap = new int[26];
+            newFreqMap[0] = freqMap[25];
+            for (int j = 1; j < 26; j++) {
+                newFreqMap[j] = freqMap[j - 1];
+            }
+            freqMap = newFreqMap;
+            // prevents overflow error here
+            freqMap[1] = (freqMap[1] + freqMap[0]) % MOD;
+        }
+
+        long sum = 0;
+        for (int val : freqMap) {
+            sum = (sum + val) % MOD;
+        }
+
+        return (int) sum;
     }
-
-    long sum = 0;
-    for (int val : freqMap) {
-        sum = (sum + val) % MOD;
-    }
-
-    return (int) sum;
-}
-
 }
